af5198e35910815526e1d41edf907fdfb31e70df
Admin <Admin@DESKTOP-R0P7LMN>
2023-03-17 20:24:16 +0530

    Commit this code 317/23

---
+++ SampleTest/src/test/java/SampleTest/SampleTest/SeleniumTest.java
@@ -0,0 +1,23 @@
+package SampleTest.SampleTest;
+
+//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumTest {
+@Test
+	public SeleniumTest() {
+		// TODO Auto-generated constructor stub
+		
+		    System.out.println("Launching google chrome with new profile..");
+	       // WebDriverManager.chromedriver().setup();
+	       // WebDriver driver = new ChromeDriver();
+	        
+	      //  driver.navigate().to("google.com");
+		    //arun
+
+}
+}
