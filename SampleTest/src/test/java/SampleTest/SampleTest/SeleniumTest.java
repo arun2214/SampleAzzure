7205fc626dbc5cebaaa22290ded2bdea72450031
Admin <Admin@DESKTOP-R0P7LMN>
2023-03-19 14:15:14 +0530

    Commit final -6-185t March

--- SampleTest/src/test/java/SampleTest/SampleTest/SeleniumTest.java
+++ SampleTest/src/test/java/SampleTest/SampleTest/SeleniumTest.java
@@ -1,22 +1,26 @@
 package SampleTest.SampleTest;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-
+import dev.failsafe.internal.util.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumTest {
 @Test
-	public SeleniumTest() {
+	public static void SeleniumTest() {
 		// TODO Auto-generated constructor stub
 		
 		    System.out.println("Launching google chrome with new profile..");
 	        //WebDriverManager.chromedriver().setup();
 	       // WebDriver driver = new ChromeDriver();
-	        
+		    Assertions.assertEquals("a", "a");       
 	        //driver.navigate().to("google.com");
 		    //arun
 
